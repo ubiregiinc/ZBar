94cbb31dffaa7c498d1e844092c943ff92fd08c2
trickart <ryotaro.seki@ubiregi.com>
2024-12-11 09:49:57 +0900

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ZBarSDK",
+    products: [
+        .library(
+            name: "ZBarSDK",
+            targets: ["ZBarSDK"]),
+    ],
+    targets: [
+        .target(
+            name: "ZBarSDK",
+            cxxSettings: [.unsafeFlags(["-fno-objc-arc"])]),
+    ]
+)
